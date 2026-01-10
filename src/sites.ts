f22c45e5529e1ffde321827672134ed28b0ef329
v-mstrs <104088833+v-mstrs@users.noreply.github.com>
2026-01-10 21:47:51 +0100

    remove webnoveltls for now

--- src/sites.ts
+++ src/sites.ts
@@ -11,13 +11,9 @@ export const SITES = [
     hostname: 'fenrirealm.com',
     contentSelector: ('.content-area')
   },
-  {
-    hostname: 'webnoveltranslations.com',
-    contentSelector: ('#novel-chapter-container')
-  }
 ] as const;
 
-export const MATCH_PATTERNS = SITES.map(s => `https://${s.hostname}/*`);
+export const MATCH_PATTERNS = SITES.map(s => `*://*.${s.hostname}/*`);
 
 export const getActiveConfig = () =>
   SITES.find(s => window.location.hostname.includes(s.hostname));
